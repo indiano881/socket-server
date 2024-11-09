b2cd840330b4c7f29a86f373dc50da3251ab03ac
davidebaldi <d.baldi88@gmail.com>
2024-11-09 11:32:46 +0100

    font DynaPuff installed and working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=DynaPuff:wght@400;700&display=swap',
+        },
+      ],
+    },
   },
   postcss: {
     plugins: {
@@ -13,4 +20,4 @@ export default defineNuxtConfig({
     },
   },
   modules: ['@nuxt/ui'],
-})
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./components/**/*.{js,vue,ts}",
+  content: [
+    "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./app.vue",
-    "./error.vue",],
+    "./error.vue",
+  ],
   theme: {
-    extend: {aspectRatio: {
-      auto: 'auto',
-      square: '1 / 1',
-      video: '16 / 9'
-    }}
+    extend: {
+      aspectRatio: {
+        auto: 'auto',
+        square: '1 / 1',
+        video: '16 / 9',
+      },
+      colors: {
+        'primary': '#756680',
+        'secondary': '#DB8CFF',
+        'accent': '#9BFF4D',
+        'mild-yellow': "#FFF14D"
+      },
+      fontFamily: {
+        dynapuff: ['DynaPuff', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
