b1330e14e4fb27ae99f2df54da984afb6dc6724f
davidebaldi <d.baldi88@gmail.com>
2024-11-19 14:50:24 +0100

    all authentichation works fine, NEED to adjsut conditial rendering and navbar expeccially logout

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <h1>log out</h1>
-        <button @click="logOut">Log Out</button>
+    <div class="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-lg mt-10 border border-gray-300 space-y-6">
+        
+        <button @click="logOut" class="w-full bg-yellow-500 text-black py-2 rounded-md hover:bg-yellow-600 transition duration-300 font-semibold">Log Out</button>
         
         <div v-if="succesMsg" class="mt-4 p-2 bg-green-100 text-green-700 rounded-md text-center">
           {{ succesMsg }}
@@ -17,12 +17,9 @@
 <script setup>
 const supabase = useSupabaseClient()
 const user = useSupabaseUser()
-const succesMsg = useState<string | boolean>(() => false)
-const errorMsg = useState<string | boolean>(() => false)
 
 const logOut= async () => {
     const {error} = await supabase.auth.signOut()
-
 
     if (error) {
         errorMsg.value= error.message
@@ -32,7 +29,16 @@ const logOut= async () => {
     succesMsg.value = "Hope to see you again!!!"
     setTimeout(async()=>{
         succesMsg.value= null;
-        await navigateTo("/")
+        await navigateTo("/login")
     }, 2000)
+
+
+    definePageMeta( {
+        middleware:
+        async () => {
+            const user= useSupabaseUser();
+            if (!user.value) return await navigateTo("/login")
+        }
+    })
 }
 </script>
