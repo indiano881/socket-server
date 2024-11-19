9e89c411323928ba1a3a52ff79f4365bb1a141e6
davidebaldi <d.baldi88@gmail.com>
2024-11-19 14:09:03 +0100

    log out logic done, need to work on middleware

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,3 +1,38 @@
 <template>
-    <h1>log out</h1>
+    <div>
+        <h1>log out</h1>
+        <button @click="logOut">Log Out</button>
+        
+        <div v-if="succesMsg" class="mt-4 p-2 bg-green-100 text-green-700 rounded-md text-center">
+          {{ succesMsg }}
+        </div>
+  
+        <div v-if="errorMsg" class="mt-4 p-2 bg-red-100 text-red-700 rounded-md text-center">
+          {{ errorMsg }}
+        </div>
+    </div>
+    
 </template>
+
+<script setup>
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+const succesMsg = useState<string | boolean>(() => false)
+const errorMsg = useState<string | boolean>(() => false)
+
+const logOut= async () => {
+    const {error} = await supabase.auth.signOut()
+
+
+    if (error) {
+        errorMsg.value= error.message
+        return
+    }
+
+    succesMsg.value = "Hope to see you again!!!"
+    setTimeout(async()=>{
+        succesMsg.value= null;
+        await navigateTo("/")
+    }, 2000)
+}
+</script>
