6aa59e71fdcfbf207c598370c1bcd6aa8bb99f0c
davidebaldi <d.baldi88@gmail.com>
2024-11-20 19:09:31 +0100

    basic socket.io config working properly

--- components/Gameboard.vue
+++ components/Gameboard.vue
@@ -1,15 +1,35 @@
 <script setup>
 const { $socket } = useNuxtApp();
 
+// Get Supabase client and authenticated user
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+
 const sendGuess = () => {
-    $socket.emit('guess', { player: 'Player1', guess: '1234' });
+    if (!user.value) {
+        console.log('User not authenticated.');
+        return;
+    }
+
+    // Emit the guess event with user details
+    $socket.emit('guess', {
+        playerId: user.value.id, // Supabase user ID
+        playerEmail: user.value.email, // Supabase user email
+        guess: '1234' // Example guess
+    });
 };
 
+// Listen for incoming guesses
 $socket.on('newGuess', (data) => {
     console.log('New guess received:', data);
 });
+
+// Debugging: Log the user object to ensure it's working
+console.log(user);
 </script>
 
 <template>
-    <button @click="sendGuess">Send Guess</button>
+    <div>
+        <button @click="sendGuess">Send Guess</button>
+    </div>
 </template>
