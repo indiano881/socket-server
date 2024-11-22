83ee35a4a07b856269726cfd9d845fc2f35ac3fd
davidebaldi <d.baldi88@gmail.com>
2024-11-22 12:43:13 +0100

    adjusted navigation bar for better UX/UI

--- components/Logout.vue
+++ components/Logout.vue
@@ -1,7 +1,6 @@
 <template>
-    <div class="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-lg mt-10 border border-gray-300 space-y-6">
-        
-        <button @click="logOut" class="w-full bg-yellow-500 text-black py-2 rounded-md hover:bg-yellow-600 transition duration-300 font-semibold">Log Out</button>
+    <div>
+        <button @click="logOut" class=" bg-yellow-500 n1 text-black hover:bg-yellow-600 transition duration-300 font font-semibold rounded-xl border-2 border-white px-2">Log Out</button>
         
         <div v-if="succesMsg" class="mt-4 p-2 bg-green-100 text-green-700 rounded-md text-center">
           {{ succesMsg }}
